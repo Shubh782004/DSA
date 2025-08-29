c475e759e9d2cda776259691edac59d5a964a9d0
Shubh Singhal <singhalshubh0708@gmail.com>
2025-08-29 19:24:16 +0530

    Time: 47 ms (28.94%) | Memory: 75.2 MB (18.94%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -7,12 +7,12 @@ public:
         unordered_map<int,int>mpp;
         while(r<n){
             mpp[fruits[r]]++;
-            while(mpp.size()>2 && l<=r){
+            if(mpp.size()>2 && l<=r){
                 mpp[fruits[l]]--;
                 if(mpp[fruits[l]]==0) mpp.erase(fruits[l]);
                 l++;
             }
-            maxlen=max(maxlen,r-l+1);
+            if(mpp.size()<=2) maxlen=max(maxlen,r-l+1);
             r++;
         }
         return maxlen;
