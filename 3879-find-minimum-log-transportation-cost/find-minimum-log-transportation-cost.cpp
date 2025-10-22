c5c0a942b7aaa365bd2e6908b5ffdc7272154fe7
Shubh Singhal <singhalshubh0708@gmail.com>
2025-10-22 22:30:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (20.32%) - LeetSync

--- 3879-find-minimum-log-transportation-cost/find-minimum-log-transportation-cost.cpp
+++ 3879-find-minimum-log-transportation-cost/find-minimum-log-transportation-cost.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     long long minCuttingCost(int n, int m, int k) {
         long long cost=0;
-        if(k<n ){
+        if(k<n){
             cost+=(long long)(n-k)*k;
         }
-        else if(k<m ){
+        else if(k<m){
             cost+=(long long)(m-k)*k;
         }
-        else if(k>=m && k>=n) return 0;
+
         return cost;
         
     }
