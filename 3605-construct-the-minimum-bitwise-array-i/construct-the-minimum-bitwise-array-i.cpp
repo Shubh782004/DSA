c4b53c1f6c6299af2c081ac1f4040464783af6d1
Shubh Singhal <singhalshubh0708@gmail.com>
2026-01-20 18:55:25 +0530

    Time: 0 ms (100.00%) | Memory: 25.1 MB (78.70%) - LeetSync

--- 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.cpp
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.cpp
@@ -2,21 +2,31 @@ class Solution {
 public:
     vector<int> minBitwiseArray(vector<int>& nums) {
         //Brute force Worst case - O(n*max(nums[i]))
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]==2) {
-                nums[i]=-1;
+        // for(int i=0; i<nums.size(); i++){
+        //     if(nums[i]==2) {
+        //         nums[i]=-1;
               
-            }
-            for(int ans=1; ans<nums[i]; ans++){
-                if((ans|ans+1)==nums[i]) {
-                    nums[i]=ans;
-                    break;
-                }
-            }
+        //     }
+        //     for(int ans=1; ans<nums[i]; ans++){
+        //         if((ans|ans+1)==nums[i]) {
+        //             nums[i]=ans;
+        //             break;
+        //         }
+        //     }
 
+        // }
+        // return nums;
+
+        //Optimal using observation of bits by converting left most set bit =0 in consecutive series
+
+        for(int i=0; i<nums.size();i++){
+            if(nums[i]==2) nums[i]=-1;
+            else if((nums[i]&2)==0) nums[i]=nums[i]-1;
+            else{
+                nums[i]=~(((nums[i]+1)&(~nums[i]))>>1)&nums[i];
+            }
         }
         return nums;
-
         
     }
 };
