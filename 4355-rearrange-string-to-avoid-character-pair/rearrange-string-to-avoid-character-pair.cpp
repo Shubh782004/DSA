90ab2c25d8e3278805a61038a4bee691cef4ff72
Shubh Singhal <singhalshubh0708@gmail.com>
2026-08-01 20:38:16 +0530

    Time: 4 ms (12.92%) | Memory: 9.2 MB (92.53%) - LeetSync

---
+++ 4355-rearrange-string-to-avoid-character-pair/rearrange-string-to-avoid-character-pair.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string rearrangeString(string s, char x, char y) {
+        string t="";
+        for(int i=0; i<s.size(); i++){
+            if(s[i]==y) t+=y;
+        }
+        for(int i=0; i<s.size(); i++){
+            if(s[i]!=y) t+=s[i];
+        }
+        return t;
+
+    }
+};
