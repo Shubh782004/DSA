ddf99b50f1a34dfed3ebeac0e2cd3e423b2a3b87
Shubh Singhal <singhalshubh0708@gmail.com>
2026-08-14 23:16:21 +0530

    Time: 339 ms (11.24%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT m1.machine_id, 
+ROUND(
+    AVG(
+        m2.timestamp-m1.timestamp
+    )
+    ,3
+) processing_time
+FROM Activity m1
+INNER JOIN Activity m2
+ON m1.machine_id=m2.machine_id AND m1.process_id=m2.process_id AND m1.activity_type='start'and m2.activity_type='end'
+GROUP BY m1.machine_id;
