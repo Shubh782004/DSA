8086bf9e3cfaec8c54ae49a82265210bedec5284
Shubh Singhal <singhalshubh0708@gmail.com>
2026-08-24 18:34:20 +0530

    Time: 3 ms (9.39%) | Memory: 9.4 MB (31.11%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -15,9 +15,18 @@ public:
 
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        int ans= recursion(0,0,m,n,dp);
-        return ans;
+        vector<vector<int>>dp(m,vector<int>(n,1));
+        // int ans= recursion(0,0,m,n,dp);
+        dp[m-1][n-1]=1;
+        for(int i=m-2; i>=0; i--){
+            for(int j=n-2; j>=0; j--){
+                int right=dp[i][j+1];
+                int down=dp[i+1][j];
+                dp[i][j]=right+down;
+            }
+        }
+
+        return dp[0][0];
 
         
     }
