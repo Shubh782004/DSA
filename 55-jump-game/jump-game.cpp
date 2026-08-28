6ca83f4c95b2e3117df9a062f8d53e3cecb99325
Shubh Singhal <singhalshubh0708@gmail.com>
2026-08-28 11:37:51 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (57.60%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -5,7 +5,7 @@ public:
         
         int furthest=-1;
         for(int i=0; i<nums.size()-1; i++){
-            furthest=max(furthest,nums[i]+i);
+            furthest=max(furthest,i+nums[i]);
             if(furthest<i+1) return false;
             if(furthest>=nums.size()-1) return true;
         }
