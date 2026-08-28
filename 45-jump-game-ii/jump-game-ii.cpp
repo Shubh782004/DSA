17ed5c29bfcb8b3fa3e71b2815644bb27c873931
Shubh Singhal <singhalshubh0708@gmail.com>
2026-08-28 11:48:40 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (99.94%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    int f(vector<int>& nums, int ind,vector<int>&dp){
-        if(ind==nums.size()-1) return 0;
-        int ans=1e9;
-        if(dp[ind]!=-1) return dp[ind];
-        for(int i=1;i<=nums[ind];i++ ){
-            if(i+ind<nums.size()) ans= min(ans,1+f(nums,i+ind,dp));
+    // int f(vector<int>& nums, int ind,vector<int>&dp){
+    //     if(ind==nums.size()-1) return 0;
+    //     int ans=1e9;
+    //     if(dp[ind]!=-1) return dp[ind];
+    //     for(int i=1;i<=nums[ind];i++ ){
+    //         if(i+ind<nums.size()) ans= min(ans,1+f(nums,i+ind,dp));
             
-        }
-        return dp[ind]=ans;
-    }
+    //     }
+    //     return dp[ind]=ans;
+    // }
     int jump(vector<int>& nums) {
         if(nums.size()==1) return 0;
-        vector<int>dp(nums.size(),-1);
-        
-        return f(nums,0,dp);
+        int l=0,r=0;
+        int jumps=0;
+        while(r<nums.size()-1){
+            int furthest=0;
+            for(int i=l; i<=r; i++){
+                furthest=max(furthest,i+nums[i]);
+            }
+            l=r+1;
+            r=furthest;
+            jumps++;
+        }
+        return jumps;
         
     }
 };
