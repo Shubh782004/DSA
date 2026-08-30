bc741437c1fdbb8b5bd2c0948be5290529ce9771
Shubh Singhal <singhalshubh0708@gmail.com>
2026-08-30 07:20:27 +0530

    Time: 2 ms (47.52%) | Memory: 13.3 MB (15.22%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -2,17 +2,25 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         stack<int>st;
-        unordered_map<int,int>mpp;
+        vector<int>nge(10001,-1);
         for(int i=nums2.size()-1; i>=0; i--){
-            while(!st.empty() && st.top()<=nums2[i]) st.pop();
-            if(st.empty()) mpp[nums2[i]]=-1;
-            else mpp[nums2[i]]=st.top();
+            while(!st.empty() && st.top()<=nums2[i] ){
+                st.pop();
+            }
+            if(!st.empty()) {
+                nge[nums2[i]]=st.top();
+            }
+            else{
+                nge[nums2[i]]=-1;
+            }
             st.push(nums2[i]);
+            
         }
-        vector<int>solution;
-        for(int i=0; i< nums1.size(); i++){
-            solution.push_back(mpp[nums1[i]]);
+       
+        for(int i=0; i<nums1.size(); i++){
+            nums1[i]=nge[nums1[i]];
         }
-        return solution;
+        return nums1;
+        
     }
 };
