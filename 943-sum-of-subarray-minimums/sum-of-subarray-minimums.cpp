ac39bbf3e34a3f4c4014806e04cc09246787e801
Shubh Singhal <singhalshubh0708@gmail.com>
2026-08-30 09:08:13 +0530

    Time: 86 ms (22.40%) | Memory: 193.5 MB (54.70%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,55 +1,43 @@
 class Solution {
 public:
-    vector<int> findNSE(vector<int>&arr){
-        int n= arr.size();
+    vector<int> fpsee(vector<int>& arr){
         stack<int>st;
-        vector<int>nse;
-        for(int i= n-1; i>=0; i-- ){
-            while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
-            if(st.empty()) nse.push_back(n);
-            else nse.push_back(st.top());
-            st.push(i);
-        }
-        reverse(nse.begin(), nse.end());
-
-        return nse;
-    }
-
-    vector<int> findPSEE(vector<int>&arr){
-        int n= arr.size();
-        stack<int>st;
-        vector<int>psee;
-        for(int i= 0; i<n; i++ ){
+        vector<int>psee(arr.size(),-1);
+        for(int i=0; i<arr.size(); i++){
             while(!st.empty() && arr[st.top()]>arr[i]) st.pop();
-            if(st.empty()) psee.push_back(-1);
-            else psee.push_back(st.top());
+            if(!st.empty()) psee[i]=st.top();
+            else psee[i]=-1;
             st.push(i);
         }
         return psee;
+
+    }
+    vector<int> fnse(vector<int>& arr){
+        stack<int>st;
+        vector<int>nse(arr.size(),arr.size());
+        for(int i=arr.size()-1; i>=0; i--){
+            while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
+            if(!st.empty()) nse[i]=st.top();
+            else nse[i]=arr.size();
+            st.push(i);
+        }
+        return nse;
+
     }
     int sumSubarrayMins(vector<int>& arr) {
-        int mod= 1e9+7;
-        long long sum=0;
-        int n= arr.size();
-
-        vector<int>psee;
         vector<int>nse;
-        nse= findNSE(arr);
-        psee=findPSEE(arr);
-        for(int i=0; i<n; i++){
+        nse=fnse(arr);
+        vector<int>psee;
+        psee= fpsee(arr);
+        int mod=1e9+7;
+        long long sum=0;
+        for(int i=0; i<arr.size(); i++){
             long long left= i-psee[i];
-            long long right= nse[i]-i;
+            long long right=nse[i]-i;
             sum=(sum+(left*right*arr[i])%mod)%mod;
         }
         return sum;
-        // for(int i=0; i<n; i++){
-        //     int mini=arr[i];
-        //     for(int j=i; j<n; j++){
-        //         mini=min(mini, arr[j]);
-        //         sum=(sum+mini)%mod;
-        //     }
-        // }
-        // return sum;
+
 
         
     }
